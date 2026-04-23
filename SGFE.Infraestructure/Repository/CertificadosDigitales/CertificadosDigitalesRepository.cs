7dacbd9553aaa196811e7fee82d3e689da40ef82
Atlast19 <ahleandro18@gmail.com>
2026-04-23 15:52:53 -0400

    Avances de la Percistencia

--- SGFE.Infraestructure/Repository/CertificadosDigitales/CertificadosDigitalesRepository.cs
+++ SGFE.Infraestructure/Repository/CertificadosDigitales/CertificadosDigitalesRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using SGFE.Domein.Entitys;
 using SGFE.Domein.Interfaces;
@@ -23,9 +24,44 @@ namespace SGFE.Percistence.Repository.CertificadosDigitales
             throw new NotImplementedException();
         }
 
-        public Task<CertificadosDigital> GetEstadoCertificadoDigalAsync(int EmpresaID)
+        public async Task<CertificadosDigital> GetEstadoCertificadoDigalAsync(int EmpresaID)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _logger.LogInformation("Ejecucion del proceso almacenado sp_CertificadosDigitales_ObtenerEstado para la empresa con ID {EmpresaID}", EmpresaID);
+
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand("sp_CertificadosDigitales_ObtenerEstado", connection))
+                    {
+                        command.CommandType = System.Data.CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@EmpresaID", EmpresaID);
+                        await connection.OpenAsync();
+                        using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                        {
+                            if (await reader.ReadAsync())
+                            {
+                                CertificadosDigital certificado = new CertificadosDigital
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                    //Activo = reader.GetString(reader.GetOrdinal("Estado")),
+                                    //FechaVencimiento = reader.IsDBNull(reader.GetOrdinal("FechaExpiracion")) ? (DateTime?)null : reader.GetDateTime(reader.GetOrdinal("FechaExpiracion"))
+                                };
+                                return certificado;
+                            }
+                            else
+                            {
+                                return null;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error al obtener el estado del certificado digital para la empresa con ID {EmpresaID}", EmpresaID);
+                throw;
+            }
         }
     }
 }
